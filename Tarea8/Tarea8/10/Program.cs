1016d17823271d3b46059982d0ac8ec968acc0ad
Jose Campos Chaves <117224557+JoseCamp1@users.noreply.github.com>
2023-05-29 12:05:01 -0600

    Tarea 8 completa

--- Tarea8/Tarea8/10/Program.cs
+++ Tarea8/Tarea8/10/Program.cs
@@ -15,6 +15,46 @@ namespace _10
             // Tarea #8 Ejercicio 10
             // Estudiante: Jose Campos Chaves
             // Módulo: POO
+            int contNinos = 0;
+            int contAdolescentes = 0;
+            int contJovenes = 0;
+            int contAdultos = 0;
+            int contAdultosMayores = 0;
+            bool OtroAsistente = true;
+
+            while (OtroAsistente)
+            {
+                Console.Write("Ingrese la edad del asistente: ");
+                int edad = Convert.ToInt32(Console.ReadLine());
+                if (edad >= 1 && edad <= 10)
+                {
+                    contNinos++;
+                }
+                else if (edad >= 11 && edad <= 15)
+                {
+                    contAdolescentes++;
+                }
+                else if (edad >= 16 && edad <= 22)
+                {
+                    contJovenes++;
+                }
+                else if (edad >= 23 && edad <= 65)
+                {
+                    contAdultos++;
+                }
+                else if (edad > 65)
+                {
+                    contAdultosMayores++;
+                }
+                Console.Write("¿Desea leer la edad de otro asistente? (S/N): ");
+                string respuesta = Console.ReadLine();
+                if (respuesta.ToUpper() != "S")
+                {
+                    OtroAsistente = false;
+                }
+            }            
+            Console.WriteLine($"Cantidad de asistentes: \nNiños (1-10 años): {contNinos}\nAdolescentes (11-15 años): {contAdolescentes}\nJóvenes (16-22 años): {contJovenes}\nAdultos (23-65 años): {contAdultos}\nAdultos mayores (más de 65 años): {contAdultosMayores}");    
+            Console.ReadKey();
         }
     }
 }
---
+++ Tarea8/Tarea8/11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11
+{
+    internal class Program
+    {
+        //11.Construya un programa que permita leer sólo números positivos hasta reunir 10 números pares,
+        //o el número 5 cuatro veces.Al final debe indicar la totalidad de números leídos.
+        // Tarea #8 Ejercicio 11
+        // Estudiante: Jose Campos Chaves
+        // Módulo: POO
+        static void Main(string[] args)
+        {
+            int contPares = 0;
+            int contCinco = 0;
+            int contTotal = 0;
+
+            while (contPares < 10 && contCinco < 4)
+            {
+                Console.Write("Ingrese un numero positivo: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num > 0)
+                {
+                    contTotal++;
+
+                    if (num % 2 == 0)
+                    {
+                        contPares++;
+                    }
+
+                    if (num == 5)
+                    {
+                        contCinco++;
+                    }
+                }
+            }
+            Console.WriteLine($"Total de números leídos: {contTotal}\nTotal de numeros pares: {contPares}\nVeces que se repitio el numero 5: {contCinco}");
+            Console.ReadKey();
+        }
+    }
+}
